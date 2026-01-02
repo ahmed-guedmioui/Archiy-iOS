e0dd458ba4b43e64b4f5cc50fc3337f3dd35ccd5
ahmed-guedmioui <ahmed.guedmioui@veltiston.ai>
2026-01-02 21:41:26 +0100

    Initial Commit

---
+++ Archiy/ArchiyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchiyApp.swift
+//  Archiy
+//
+//  Created by Ahmed Guedmioui on 2/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct ArchiyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
