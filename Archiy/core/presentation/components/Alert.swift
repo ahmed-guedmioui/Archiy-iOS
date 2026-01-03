e9f6affe5e400f17210d81f17d47b6cb13e6906b
ahmed-guedmioui <ahmed.guedmioui@veltiston.ai>
2026-01-03 15:59:55 +0100

    wip

---
+++ Archiy/core/presentation/components/Alert.swift
@@ -0,0 +1,55 @@
+//
+//  Alert.swift
+//  Archiy
+//
+//  Created by Ahmed Guedmioui on 2/1/2026.
+//
+
+import SwiftUI
+import Combine
+
+class Alert: ObservableObject {
+    static let shared = Alert()
+    
+    @Published var buttonText: String = ""
+    @Published var title: String = ""
+    @Published var message: String = ""
+    @Published var isPresented: Bool = false
+    
+    func show(buttonText: String = "OK".local, title: String = "", message: String = "") {
+        self.buttonText = buttonText
+        self.title = title
+        self.message = message
+        self.isPresented = true
+    }
+    
+    func dismiss() {
+        self.isPresented = false
+    }
+}
+
+struct AlertModifier: ViewModifier {
+    @ObservedObject var alert = Alert.shared
+    
+    func body(content: Content) -> some View {
+        content
+            .alert(
+                alert.title,
+                isPresented: $alert.isPresented,
+                actions: {
+                    Button(alert.buttonText) {
+                        alert.dismiss()
+                    }
+                },
+                message: {
+                    Text(alert.message)
+                }
+            )
+    }
+}
+
+extension View {
+    func withAlert() -> some View {
+        self.modifier(AlertModifier())
+    }
+}
